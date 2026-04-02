a817d3cf69bc554f26edfd9f37e9d678de0844be
educatorsRlearners <p.evansimpson@gmail.com>
2026-04-02 17:15:24 +0200

    Feature: Updates table definition

--- create_tables.py
+++ create_tables.py
@@ -1,18 +1,21 @@
 import psycopg2
 from config import load_config
 
+
 def create_tables():
-    """ create tables in the PostgreSQL database"""
+    """create tables in the PostgreSQL database"""
     commands = (
         """
         CREATE TABLE IF NOT EXISTS stores (
-            store_id SERIAL PRIMARY KEY,
-            store_name VARCHAR(255) NOT NULL,
-            address VARCHAR(255) NOT NULL,
-            city VARCHAR(255) NOT NULL,
-            state VARCHAR(255) NOT NULL,
-            zip_code VARCHAR(20) NOT NULL,
-            phone_number VARCHAR(20) NOT NULL
+            id SERIAL PRIMARY Key
+            , store_number INTEGER
+            , store_name TEXT NOT NULL
+            , street TEXT NOT NULL
+            , city TEXT NOT NULL
+            , state VARCHAR(2) NOT NULL
+            , zip_code VARCHAR(10) NOT NULL
+            , phone_number VARCHAR(20) NOT NULL
+            , url TEXT NOT NULL
         )
         """,
     )
@@ -28,10 +31,14 @@ def create_tables():
                 # create table one by one
                 for command in commands:
                     cur.execute(command)
-                print("Tables created successfully.")
+                print("Table created successfully.")
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
         if conn is not None:
             conn.close()
             print("Database connection closed.")
+
+
+if __name__ == "__main__":
+    create_tables()
