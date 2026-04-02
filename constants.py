6781ba5872cee1865767fd75abe311d8e1288b24
educatorsRlearners <p.evansimpson@gmail.com>
2026-04-02 18:22:06 +0200

    Feature: Creates file to hold all constants

---
+++ constants.py
@@ -0,0 +1,13 @@
+BASE_URL = "https://locations.traderjoes.com"
+BLANK = "Blank"
+COMING_SOON = "Coming Soon"
+STORE_COLUMNS = [
+    "store_number",
+    "store_name",
+    "street",
+    "city",
+    "state",
+    "zip_code",
+    "phone_number",
+    "url",
+]
