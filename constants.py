d7579ab3b9eaae9d125cf7337ce3ed1c73806478
educatorsRlearners <p.evansimpson@gmail.com>
2026-04-03 18:48:19 +0200

    Feature: Updated constraints

--- constants.py
+++ constants.py
@@ -9,6 +9,10 @@ COMING_SOON = "Coming Soon"
 CREATE_STORES_SQL = "sql/create_stores.sql"
 CREATE_INTERNET_SPEEDS_SQL = "sql/create_internet_speeds.sql"
 
+# Table Names
+STORES_TABLE = "stores"
+INTERNET_SPEED_TABLE = "internet_speeds"
+
 STORE_COLUMNS = [
     "store_number",
     "store_name",
@@ -18,4 +22,31 @@ STORE_COLUMNS = [
     "zip_code",
     "phone_number",
     "url",
+]
+
+
+INTERNET_SPEED_COLUMNS = [
+    "zip",
+    "population",
+    "county",
+    "state",
+    "wiredcount_2020",
+    "fwcount_2020",
+    "allprovidercount_2020",
+    "wired25_3_2020",
+    "wired100_3_2020",
+    "all25_3_2020",
+    "all100_3_2020",
+    "testcount",
+    "averagembps",
+    "fastestaveragembps",
+    "access_terrestrial_broadband",
+    "lowest_priced_terrestrial_broadband",
+    "wiredcount_2015",
+    "fwcount_2015",
+    "allprovidercount_2015",
+    "wired25_3_2015",
+    "wired100_3_2015",
+    "all25_3_2015",
+    "all100_3_1",
 ]
